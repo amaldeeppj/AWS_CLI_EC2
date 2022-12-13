e36b8ef615e278bbb769740551e8cf6402836d20
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-12-13 20:17:37 +0530

    userdata

---
+++ userdata1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define hostname
+HN=mumbai.amaldeep.tech
+
+yum update -y
+hostnamectl set-hostname $HN
+yum install git httpd -y 
+amazon-linux-extras install php7.4  -y 
+systemctl enable httpd.service
+git clone https://github.com/amaldeeppj/webserver_sample_content_v1.git /var/website/
+cp -r  /var/website/*  /var/www/html/
+chown -R apache:apache /var/www/html/
+systemctl restart httpd.service
